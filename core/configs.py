0aa4f357e1043815ca7ed4ad0e4f3dea352ddfb5
Diego Alencar Jacober <57761933898>
2023-04-03 13:35:29 -0300

    Configuring core module

---
+++ core/configs.py
@@ -0,0 +1,17 @@
+from pydantic import BaseSettings
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class Settings(BaseSettings):
+    """
+     Configurações gerais para a aplicação
+    """
+    API_V1_STR: str = '/api/v1'
+    DB_URL: str = "mysql+asyncmy://root:#@localhost:3306/interclasse"
+    DB_BASE_MODEL = declarative_base()
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
