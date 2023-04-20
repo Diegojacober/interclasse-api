5d6fb8e73cb2e1f68bb1bcb17881dd92ca74b107
Diego <diegoalencar.jacober@gmail.ocm>
2023-04-19 21:26:46 -0300

    Conectando na nuvem

--- core/configs.py
+++ core/configs.py
@@ -7,7 +7,10 @@ class Settings(BaseSettings):
      Configurações gerais para a aplicação
     """
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = "mysql+asyncmy://root:@localhost:3306/interclasse"
+    
+    #conexão local
+    # DB_URL: str = "mysql+asyncmy://root:Di29122004#@localhost:3306/interclasse"
+    DB_URL: str = "mysql+asyncmy://diegos@interclasse-2023:senaimange2023@interclasse-2023.mariadb.database.azure.com:3306/interclasse"
     DB_BASE_MODEL = declarative_base()
 
     class Config:
