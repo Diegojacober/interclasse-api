0710073227732918b4e61f4fa2bdbf3d5f2c0c13
Diego <diegoalencar.jacober@gmail.ocm>
2023-04-21 17:36:15 -0300

    Fixing bugs route

--- api/v1/api.py
+++ api/v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from api.v1.endpoints import curso, modalidade, atleta, time, jogo
+from api.v1.endpoints import curso, modalidade, atleta, time, jogo,campeao
 
 api_router = APIRouter()
 
@@ -9,6 +9,4 @@ api_router.include_router(modalidade.router, prefix='/modalidades', tags=['Rotas
 api_router.include_router(atleta.router, prefix='/atletas', tags=['Rotas dos atletas'])
 api_router.include_router(time.router, prefix='/times', tags=['Rotas dos times'])
 api_router.include_router(jogo.router, prefix='/jogos', tags=['Rotas dos jogos'])
-
-
-#api/v1/cursos
+api_router.include_router(campeao.router, prefix='/campeao', tags=['Rotas dos campeões'])
